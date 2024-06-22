6a14a33c81a7bd9be6525ad5193812152908e3d1
Roman Berezkin <crazzzy_coder@mail.ru>
2024-06-22 21:57:42 +0300

    Complement comments about themes

--- lib/uikit/theme.dart
+++ lib/uikit/theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 // TODO: Define colors after basic layout is done
+// now we only have light theme hardcoded for the current homework
 abstract class AppThemeData {
   static final lightTheme = ThemeData();
 
