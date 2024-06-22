49a4c5c783474c31c2ea2dfbc9da5f703ade1a16
Roman Berezkin <crazzzy_coder@mail.ru>
2024-06-22 14:19:46 +0300

    Fix design colors of text

--- lib/uikit/app_text_style.dart
+++ lib/uikit/app_text_style.dart
@@ -6,41 +6,55 @@ import 'package:yet_another_todo/uikit/colors.dart';
 /// App Text Style contains all
 /// typography styles used in the app
 enum AppTextStyle {
-  largeTitle(TextStyle(
-    fontSize: 32.0,
-    height: 32 / 38,
-    fontWeight: FontWeight.w500,
-    fontFamily: "Roboto",
-  )),
+  largeTitle(
+    TextStyle(
+      fontSize: 32.0,
+      height: 32 / 38,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Roboto",
+      color: ColorPalette.lightLabelPrimary,
+    ),
+  ),
 
-  mediumTitle(TextStyle(
-    fontSize: 20.0,
-    height: 20 / 32,
-    fontWeight: FontWeight.w500,
-    fontFamily: "Roboto",
-  )),
+  mediumTitle(
+    TextStyle(
+      fontSize: 20.0,
+      height: 20 / 32,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Roboto",
+      color: ColorPalette.lightLabelPrimary,
+    ),
+  ),
 
-  buttonText(TextStyle(
-    fontSize: 14.0,
-    height: 14 / 24,
-    fontWeight: FontWeight.w500,
-    fontFamily: "Roboto",
-  )),
+  buttonText(
+    TextStyle(
+      fontSize: 14.0,
+      height: 14 / 24,
+      fontWeight: FontWeight.w500,
+      fontFamily: "Roboto",
+      color: ColorPalette.lightLabelPrimary,
+    ),
+  ),
 
-  bodyText(TextStyle(
-    fontSize: 16.0,
-    height: 16 / 20,
-    fontWeight: FontWeight.w400,
-    fontFamily: "Roboto",
-  )),
+  bodyText(
+    TextStyle(
+      fontSize: 16.0,
+      height: 16 / 20,
+      fontWeight: FontWeight.w400,
+      fontFamily: "Roboto",
+      color: ColorPalette.lightLabelPrimary,
+    ),
+  ),
 
-  subheadText(TextStyle(
-    fontSize: 14.0,
-    height: 14 / 20,
-    fontFamily: "Roboto",
-    fontWeight: FontWeight.w400,
-    color: ColorPalette.lightLabelTertiary,
-  ));
+  subheadText(
+    TextStyle(
+      fontSize: 14.0,
+      height: 14 / 20,
+      fontFamily: "Roboto",
+      fontWeight: FontWeight.w400,
+      color: ColorPalette.lightLabelTertiary,
+    ),
+  );
 
   final TextStyle value;
 
