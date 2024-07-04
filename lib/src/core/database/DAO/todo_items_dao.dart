25a59553b60644eb5a0a15f73329802539a8efa3
Roman Berezkin <crazzzy_coder@mail.ru>
2024-07-04 04:00:21 +0300

    Add batch todo insertion for database

--- lib/src/core/database/DAO/todo_items_dao.dart
+++ lib/src/core/database/DAO/todo_items_dao.dart
@@ -8,34 +8,42 @@ part 'todo_items_dao.g.dart';
 class TodoDao extends DatabaseAccessor<AppDatabaseImpl> with _$TodoDaoMixin {
   final AppDatabaseImpl db;
 
-  // Called by the AppDatabase class
   TodoDao(this.db) : super(db);
 
-  // CRUD Operations
+  Future<void> insertTodoItem(TodoItem todoItem) async =>
+      into(todoItems).insert(
+        todoItem,
+        mode: InsertMode.insertOrReplace,
+      );
 
-  // Create
-  Future<void> insertTodoItem(TodoItem todoItem) =>
-      into(todoItems).insert(todoItem, mode: InsertMode.insertOrReplace);
+  Future<void> insertTodoItems(List<TodoItem> itemsToInsert) async =>
+      batch((batch) {
+        batch.insertAll(
+          todoItems,
+          itemsToInsert,
+          mode: InsertMode.insertOrReplace,
+        );
+      });
 
   // Read
-  Future<List<TodoItem>> getAllTodoItems() => select(todoItems).get();
+  Future<List<TodoItem>> getAllTodoItems() async => select(todoItems).get();
 
-  Future<TodoItem?> getTodoById(String id) => (select(todoItems)
+  Future<TodoItem?> getTodoById(String id) async => (select(todoItems)
         ..where(
           (todo) => todo.id.equals(id),
         ))
       .getSingleOrNull();
 
   // Update
-  Future<bool> updateTodoItem(TodoItem todoItem) =>
+  Future<bool> updateTodoItem(TodoItem todoItem) async =>
       update(todoItems).replace(todoItem);
 
   // Delete
-  Future<int> deleteTodoItem(TodoItem todoItem) =>
+  Future<int> deleteTodoItem(TodoItem todoItem) async =>
       delete(todoItems).delete(todoItem);
 
-  Future<int> deleteAll() => delete(todoItems).go();
+  Future<int> deleteAll() async => delete(todoItems).go();
 
-  Future<void> deleteByTodoId(String id) =>
+  Future<void> deleteByTodoId(String id) async =>
       (delete(todoItems)..where((tbl) => tbl.id.equals(id))).go();
 }
