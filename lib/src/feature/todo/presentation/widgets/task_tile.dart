9c3f87666786f8737a790fc400622a4eb2115d90
Roman Berezkin <crazzzy_coder@mail.ru>
2024-07-11 00:58:49 +0300

    Fix UI list tile with RichText, so there is no overflow
    Moreover, now we can control todo lines count.

--- lib/src/feature/todo/presentation/widgets/task_tile.dart
+++ lib/src/feature/todo/presentation/widgets/task_tile.dart
@@ -102,32 +102,44 @@ class _TaskTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        if (task.priority == TaskPriority.important)
-          Padding(
-            padding: const EdgeInsets.only(right: 4),
-            child: SvgPicture.asset(
-              'assets/icons/exclamation_marks.svg',
-            ),
-          ),
-        if (task.priority == TaskPriority.low)
-          Padding(
-            padding: const EdgeInsets.only(right: 4),
-            child: SvgPicture.asset(
-              'assets/icons/arrow_down.svg',
-            ),
-          ),
-        Text(
-          task.description,
-          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                decoration: task.isDone ? TextDecoration.lineThrough : null,
-                color:
-                    task.isDone ? Theme.of(context).colorScheme.tertiary : null,
+    final theme = Theme.of(context);
+
+    return RichText(
+      maxLines: 3,
+      overflow: TextOverflow.ellipsis,
+      text: TextSpan(
+        children: [
+          if (task.priority == TaskPriority.important)
+            WidgetSpan(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 4),
+                child: SvgPicture.asset(
+                  'assets/icons/exclamation_marks.svg',
+                  width: 16,
+                  height: 16,
+                ),
               ),
-        ),
-      ],
+            ),
+          if (task.priority == TaskPriority.low)
+            WidgetSpan(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 4),
+                child: SvgPicture.asset(
+                  'assets/icons/arrow_down.svg',
+                  width: 16,
+                  height: 16,
+                ),
+              ),
+            ),
+          TextSpan(
+            text: task.description,
+            style: theme.textTheme.bodyMedium?.copyWith(
+              decoration: task.isDone ? TextDecoration.lineThrough : null,
+              color: task.isDone ? theme.colorScheme.tertiary : null,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
