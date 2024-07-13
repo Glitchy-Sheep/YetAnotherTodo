cc5a938d14b9ab64447199e096af75d117c9dbd7
Roman Berezkin <crazzzy_coder@mail.ru>
2024-07-13 19:23:45 +0300

    Fix todo db repository interface

--- lib/src/feature/todo/domain/repository/db_todo_repository.dart
+++ lib/src/feature/todo/domain/repository/db_todo_repository.dart
@@ -3,8 +3,8 @@ import '../entities/task_entity.dart';
 abstract interface class TodoDbRepository {
   Future<List<TaskEntity>> getTodos();
   Future<TaskEntity?> getTodoById(String id);
+  Future<TaskEntity> addTodo(TaskEntity todo);
 
-  Future<void> addTodo(TaskEntity todo);
   Future<void> editTodo(String id, TaskEntity todo);
   Future<void> deleteTodo(String id);
   Future<void> deleteAllTodos();
