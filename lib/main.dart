09c209f3d3264c769a8639dbd7a37083a4a7495c
Roman Berezkin <crazzzy_coder@mail.ru>
2024-07-13 12:12:42 +0300

    More accurate main logger

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ import 'src/core/database/database_impl.dart';
 import 'src/core/tools/logger.dart';
 import 'src/feature/app/app_entry_point.dart';
 
+const _loggerPrefix = '[MAIN]';
+
 void main() {
   PlatformDispatcher.instance.onError = (error, stackTrace) {
     logger.e('$error \n $stackTrace \n');
@@ -24,16 +26,16 @@ void main() {
       WidgetsFlutterBinding.ensureInitialized();
 
       await dotenv.load(fileName: 'config.env');
-      logger.i('ENV FILE LOADED');
+      logger.i('$_loggerPrefix: .env config file loaded');
 
       final sharedPrefs = await SharedPreferences.getInstance();
+      logger.i('$_loggerPrefix: Shared preferences initialized');
+
       final db = AppDatabaseImpl();
+      logger.i('$_loggerPrefix: Database initialized');
 
       // App entry point
-      runApp(YetAnotherTodoApp(
-        sharedPrefs: sharedPrefs,
-        db: db
-      ));
+      runApp(YetAnotherTodoApp(sharedPrefs: sharedPrefs, db: db));
     },
     (error, stackTrace) {
       logger.e('$error \n $stackTrace \n');
